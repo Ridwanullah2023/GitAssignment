eb0f91487d5a46c054e2b68eb8846d54a3924392
unknown <Ridwanullah2023>
2023-12-07 09:19:51 +1000

    new change

---
+++ Menu/ForGiTTest.cs
@@ -0,0 +1,11 @@
+namespace Library_Management_System.Menu
+{
+    public class ForGiTTest
+    {
+        public void Print()
+        {
+          System.Console.WriteLine("This, right here is a new change");
+
+        }
+    }
+}
